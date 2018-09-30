eb2162982db44455e3c7cfac798b9739ff3b6968
Alex Kennedy <akennedy4155@gmail.com>
2018-09-30 08:08:59 -0700

    Initial commit.

---
+++ main_scrape.py
@@ -0,0 +1,26 @@
+import url_tools
+import re
+
+#URLs for various pages as strings
+couch_url = "https://www.ikea.com/us/en/catalog/categories/departments/living_room/39130/"
+samplecouch = "https://www.ikea.com/us/en/catalog/products/S19150508/"
+
+
+
+#Soupify from URL
+all_couches_soup = url_tools.get_soup_from_url(couch_url)
+sample_couch_soup = url_tools.get_soup_from_url(samplecouch)
+
+#extracting the data, sample...
+#gets all of the product titles and
+products = all_couches_soup.findAll("div", class_="threeColumn product ")
+for p in products:
+    title = p.find("span", class_="productTitle").text
+    short_description = p.find("span", class_="productDesp").text
+    print(title,": ", short_description, "Colors: ", sep = "")
+
+url_tools.get_cache_file_name(couch_url)
+
+
+# for n in range(0,2):
+#     print(products[n].prettify())
