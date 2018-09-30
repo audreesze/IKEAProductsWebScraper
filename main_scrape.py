bcc0cb11e62a70eeed480db3c67b83d330d64de2
Alex Kennedy <akennedy4155@gmail.com>
2018-09-30 08:35:48 -0700

    Fixed changes back to version 1

--- main_scrape.py
+++ main_scrape.py
@@ -1,3 +1,4 @@
+import os
 import url_tools
 import re
 
@@ -5,7 +6,9 @@ import re
 couch_url = "https://www.ikea.com/us/en/catalog/categories/departments/living_room/39130/"
 samplecouch = "https://www.ikea.com/us/en/catalog/products/S19150508/"
 
-
+#Create the html_cache directory if it doesn't already exists
+if not os.path.exists("html_cache"):
+    os.makedirs("html_cache")
 
 #Soupify from URL
 all_couches_soup = url_tools.get_soup_from_url(couch_url)
@@ -13,12 +16,12 @@ sample_couch_soup = url_tools.get_soup_from_url(samplecouch)
 
 #prints the basic details of the couches to the console
 products = all_couches_soup.findAll("div", class_="threeColumn product ")
-# for p in products:
-#     title = p.find("span", class_="productTitle").text
-#     short_description = p.find("span", class_="productDesp").text
-#     print(title,": ", short_description, "Colors: ", sep = "")
+for p in products:
+    title = p.find("span", class_="productTitle").text
+    short_description = p.find("span", class_="productDesp").text
+    print(title,": ", short_description, "Colors: ", sep = "")
 
-print(p[0].prettify())
+# print(products[0].prettify())
 
 #IN COUCHES:
 #Name
