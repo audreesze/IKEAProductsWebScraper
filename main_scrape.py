52f455d89a858b624ab998c4114cce0582fb4925
Alex Kennedy <akennedy4155@gmail.com>
2018-09-30 09:01:31 -0700

    Making changes to the way that we get the information about the product. Trying to move up the tree from individual products -> category -> bigger category -> whole site etc.

--- main_scrape.py
+++ main_scrape.py
@@ -16,12 +16,27 @@ sample_couch_soup = url_tools.get_soup_from_url(samplecouch)
 
 #prints the basic details of the couches to the console
 products = all_couches_soup.findAll("div", class_="threeColumn product ")
-for p in products:
-    title = p.find("span", class_="productTitle").text
-    short_description = p.find("span", class_="productDesp").text
-    print(title,": ", short_description, "Colors: ", sep = "")
+# for p in products:
+#     title = p.find("span", class_="productTitle").text
+#     short_description = p.find("span", class_="productDesp").text
+#     print(title,": ", short_description, "Colors: ", sep = "")
 
-# print(products[0].prettify())
+p = products[0]
+# print(p.prettify())
+couch_url = "https://www.ikea.com" + p.findAll("a", class_="productLink")[0]['href']
+# print(couch_url)
+couch_soup = url_tools.get_soup_from_url(couch_url)
+# print(couch_soup.prettify())
+name = p.findAll("span", class_ = "productTitle")[0].text
+details = p.findAll("h1")
+print(couch_soup.prettify())
+
+#for each product on this page:
+#Go to the URL of the product and pull the information below, then write to a
+#product object
+# for p in products:
+#     couch_url = all_couches_soup.findAll("")
+#     current_couch_soup = url_tools.get_soup_from_url(couch_url)
 
 #IN COUCHES:
 #Name
@@ -31,3 +46,4 @@ for p in products:
 #Image URL
 #Review
 #Article ID
+#Category
